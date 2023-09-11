8952ac56af302a92d19e51f653ab0514e6cb2110
Sumu Vuori <sumu.vuori@tuni.fi>
2023-09-12 00:41:56 +0300

    15

--- homework/02/13-15/index.js
+++ homework/02/13-15/index.js
@@ -2,6 +2,7 @@ const axios = require("axios");
 const process = require("process");
 
 function getNames(x = 0) {
+  // defaults to 0
   if (isNaN(x)) {
     x = 0;
   }
@@ -12,14 +13,26 @@ function getNames(x = 0) {
       .get("https://swapi.dev/api/people")
       .then((resp) => {
         for (let i = 0; i < resp.data.results.length; i++) {
-          characters.push(resp.data.results[i]);
+          let chara = resp.data.results[i];
+
+          // calculates bmi
+          chara.bmi =
+            chara.mass / ((chara.height / 100) * (chara.height / 100));
+          characters.push(chara);
         }
       })
       .finally(() => {
+        // sorts by bmi
+        characters.sort((a, b) => {
+          return a.bmi > b.bmi ? 1 : -1;
+        });
+
+        // maps characters
         const charactersMapped = characters.map(
           (c) => `<li> ${c.name} </li>\n`
         );
 
+        // adds all to string
         let listToString = "<ul>\n";
         for (let i = 0; i < charactersMapped.length; i++) {
           listToString += charactersMapped[i];
@@ -29,6 +42,7 @@ function getNames(x = 0) {
         console.log(listToString);
       });
   } else {
+    // return name if argument was given (e13)
     axios.get("https://swapi.dev/api/people/" + x).then((resp) => {
       console.log(resp.data.name);
     });
